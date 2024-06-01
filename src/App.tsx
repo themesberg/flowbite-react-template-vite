8b11ccebc9b139428c20c8e99a137b8f8c6cc71a
koridal <hjpark002@gmail.com>
2024-06-01 12:24:07 +0900

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
