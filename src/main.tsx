7d0a02db6a21c1ebe91aecb85a8ede8645be4c20
SutuSebastian <sebiitv@gmail.com>
2025-04-15 08:38:07 +0300

    persist theme mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -8,3 +9,5 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
+
+initThemeMode();
