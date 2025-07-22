d802ca6066e8c8c56415726159f7d3761dbeab09
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:07:59 +0300

    up packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeInit } from "../.flowbite-react/init";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ThemeInit />
     <App />
   </StrictMode>,
 );
